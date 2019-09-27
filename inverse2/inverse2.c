8b1ce60a8cf4ebe9a36b44ba28461a4bcb96e14b
vats9549 <55613787+vats9549@users.noreply.github.com>
2019-09-28 00:21:40 +0530

    Taskphase2ques

---
+++ inverse2/inverse2.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+int main()
+{int arr[100][100],arr1[100][100],arrc[100][100];
+int i,j,k,n,x,q,a;
+printf("Enter order of the matrix\n");
+scanf("%d\n",&n);
+printf("Enter the elements of the matrix\n");
+for(i=0;i<n;i++)
+{for(j=0;j<n;j++)
+{scanf("%d",&arr[i][j]);}
+}
+a=n-1;
+for(i=0;i<n;i++)
+{for(j=0;j<n;j++)
+{printf("%d\t",arr[i][j]);}
+printf("\n");}
+x = det(arr,n);
+for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {for(k=0;k<n;k++)
+    {
+        for(q=0;q<n;q++)
+        {
+            if(k!=i&&j!=q)
+            {
+                arr1[k][q]=arr[i][j];
+            }
+        }
+
+det(arr1,a)=arrc[j][i];
+    }
+
+    }
+    for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+    printf("%d\t",(arrc[i][j]/det(arr,n))
+}
+}}}
+int det(int arr[100][100], int n)
+{int Minor[100][100];
+int i,j,k,t,z;
+int detr;
+int c[100];
+int l=1;
+detr = 0;
+if(n == 2)
+{detr = arr[0][0]*arr[1][1]-arr[0][1]*arr[1][0];
+ return detr;}
+else
+{for(i = 0 ; i < n ; i++)
+{t=0,z=0;
+for(j = 0 ; j < n ; j++)
+{for(k = 0 ; k < n ; k++)
+{if(j!=0 && k != i)
+{Minor[t][z] = arr[j][k];
+z++;
+if(z>n-2)
+{t++;
+z=0;}}}}
+detr = detr + l*(arr[0][i]*det(Minor,n-1));
+l=-1*l;
+}}
+return detr;
+}
